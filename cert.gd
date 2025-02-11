49a4ad41e8a8b15ab5304912d05a6a6f4f535d95
SoyKhaler <74861088+soykhaler@users.noreply.github.com>
2025-02-11 13:19:06 +0100

    cambios hechos

--- cert.gd
+++ cert.gd
@@ -4,7 +4,6 @@ var image : Image = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print("inicializar escena")
 	render();
 	pass # Replace with function body.
 
