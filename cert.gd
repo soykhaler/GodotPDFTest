be8cb8749e5f8a08cbe3ddf607b1eea8e470e67c
SoyKhaler <74861088+soykhaler@users.noreply.github.com>
2025-02-11 13:21:16 +0100

    eliminar lineas pass

--- cert.gd
+++ cert.gd
@@ -5,12 +5,10 @@ var image : Image = null;
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	render();
-	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
 
 
 func render():
